02267c11544372e914250a29aadfaa33e53ce8a0
Adler11th <mgoftest@gmail.com>
2018-02-02 19:31:14 -0800

    v0.2

--- assets/jscript/game.js
+++ assets/jscript/game.js
@@ -1,37 +1,79 @@
-//--variables
+document.addEventListener("DOMContentLoaded",function(event){
 
 var dictionary=["hello", "messy","pygama","frenzy"];
-var word = dictionary[Math.floor(Math.random()*4)];
-var e_word=[];
-var tries = 10;
-console.log(word);
+// --Functions
+//=============================================================
 
-//--function that 
+//TODO function Reset()
 
-function Replace(string_one,string_two){
+function Replace(string_one){
     for(index = 0; index<string_one.length;index++){
-        e_word[index]="_";
+        utilityArray[index]="_";
+        Output(utilityArray);
     }
-    console.log(e_word);
+}
+
+//--Output function
+
+function Output(string_one){
+    document.getElementById("output").textContent="";
+    string_one.forEach(function(element){
+        document.getElementById("output").textContent+=element+" ";
+    })
 }
 
 //--function that checks if the letter exsists in the word
-
-function Check(string,char){
-    for(index = 0; index < string.length; index++){
-        if(char == string[index]){
-            console.log("-success");
-        }else{
-            console.log("-fail")
+ 
+function CheckLetter(string_a,char,string_b){
+    for(index = 0; index < string_a.length; index++){
+        if(char == string_a[index]){
+            string_b[index]=string_a[index];
         }
     }
 }
 
-//--event calls for functions
-Replace(word,e_word);
+//--function that checks win/loose conditions
 
+function CheckWinLoose(string_a){
+    var number = 0;
+    string_a.forEach(function(element){
+        if(element!="_"){
+            number++;
+        }
+    })
+
+    if(number==string_a.length){
+        return true;
+    }else{
+        return false;
+    }
+}
+
+
+//--main()
+//--game loop
+// ===================================================================
+
+var word = dictionary[Math.floor(Math.random()*4)];
+console.log(word);
+
+var utilityArray=[];//side array to track users guessed letters
+
+var keys = "";//--variable that registers eneter keys
+var tries = 10;
+
+Replace(word);
+document.getElementById("tries").textContent = tries;
 document.onkeyup = function(event){
     var letter = event.key;
-    Check(word,letter);
-    document.getElementById("guessed").textContent += letter+", ";
+    if(keys.indexOf(letter)==-1){
+    keys+=letter;
+    CheckLetter(word,letter,utilityArray);
+    //TODO track tries
+    Output(utilityArray);
+    document.getElementById("tries").textContent = tries;
+    document.getElementById("guessed").textContent += letter+" ";
+    if(CheckWinLoose(utilityArray)){alert("You won!");}
+    }
 }
+})
